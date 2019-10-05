969ee3d2662efcdbd33d40023d97e6a2a29f667c
once-ler <htao@eztier.com>
2019-10-05 09:14:07 -0400

    Start endpoints

--- src/main/scala/com/eztier/testhttp4sdoobie/Server.scala
+++ src/main/scala/com/eztier/testhttp4sdoobie/Server.scala
@@ -2,14 +2,27 @@ package com.eztier.testhttp4sdoobie
 
 import cats.effect._
 import cats.syntax.all._
-
+import com.eztier.testhttp4sdoobie.infrastructure.endpoint.AuthorEndpoints
+import org.http4s.server.{Router, Server => H4Server}
 import domain.authors._
-
-import infrastructure.repository.doobie.{DoobieAuthorRepositoryInterpreter}
+import infrastructure.repository.doobie.DoobieAuthorRepositoryInterpreter
 
 object Server extends IOApp {
-  
+
+  def createServer[F[_]: ContextShift: ConcurrentEffect: Timer]: Resource[F, H4Server[F]] =
+    val httpApp = Router(
+      "/authors" -> AuthorEndpoints.endpoints[F](authorService)
+    ).orNotFound
+
+    for {
+
+    } yield server
+
   def run(args: List[String]): IO[ExitCode] =
+
+
+
+
     args.headOption match {
       case Some(name) =>
         IO(println(s"Hello, $name.")).as(ExitCode.Success)
